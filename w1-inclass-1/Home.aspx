36aadba3efe6005f44c7e5f0f66a326fc37822af
Jonathan <jdoolittle126@gmail.com>
2021-10-07 22:18:04 -0400

    Week 1

---
+++ w1-inclass-1/Home.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="w1_inclass_1.Home" ValidateRequest="false"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <h1>XML INPUT</h1>
+                <asp:TextBox runat="server" ID="textBoxXmlInput" TextMode="MultiLine"></asp:TextBox>
+            </div>
+
+            <div>
+                <h1>XPATH FUNCTIONS</h1>
+                <ol>
+                    <li><asp:Button runat="server" Name="ButtonDisplayDateVersion" OnClick="ButtonDisplayDateVersion_Click" Text="Display the version date and version number"/></li>
+                    <li><asp:Button runat="server" Name="ButtonInfoOnContact" OnClick="ButtonInfoOnContact_Click" Text="Return all of the information about each contact (including the sex)"/></li>
+                    <li><asp:Button runat="server" Name="ButtonOnlyMaleInfo" OnClick="ButtonOnlyMaleInfo_Click" Text="Return all of the information for only the males in the contact list"/></li>
+                    <li>
+                        <asp:TextBox runat="server" Name="TextBoxSpecificLastName" />
+                        <asp:Button runat="server" Name="ButtonSpecificLastName" OnClick="ButtonSpecificLastName_Click" Text="Return all of the information for a specific last name that you enter "/>
+                    </li>
+                    <li>
+                        <asp:Button runat="server" OnClick="ButtonClearDisplay_Click" Text="Clear Display"/>
+                    </li>
+                </ol>
+            </div>
+
+            <div>
+                <h1>OUTPUT</h1>
+                <asp:TextBox runat="server" ID="textBoxOutput" ReadOnly="true" TextMode="MultiLine" />
+            </div>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ w1-inclass-1/Home.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.XPath;
+
+namespace w1_inclass_1
+{
+    public partial class Home : System.Web.UI.Page
+    {
+
+        XPathNavigator navigator;
+        XPathDocument document;
+        XPathNodeIterator nodeIterator;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                
+        }
+        protected bool TryLoadXml(String xPath)
+        {
+            try
+            {
+                using (StringReader stringReader = new StringReader(textBoxXmlInput.Text))
+                {
+                    document = new XPathDocument(stringReader);
+                    navigator = document.CreateNavigator();
+                }
+            } catch (Exception)
+            {
+                textBoxOutput.Text = "Invalid XML!";
+                return false;
+            }
+            return true;
+        }
+
+        protected void ExecuteXPath(String xPath)
+        {
+            if (TryLoadXml("//version[1]/*"))
+            {
+                nodeIterator = navigator.Select(xPath);
+                textBoxOutput.Text += $"\n ==== [QUERY = \"{xPath}\"] ===\n";
+                while (nodeIterator.MoveNext())
+                {
+                    textBoxOutput.Text += nodeIterator.Current + "\n";
+                }
+            }
+
+        }
+
+        protected void ButtonClearDisplay_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = "";
+        }
+
+        protected void ButtonDisplayDateVersion_Click(object sender, EventArgs e)
+        {
+            ExecuteXPath("//version[1]/*");
+        }
+
+        protected void ButtonInfoOnContact_Click(object sender, EventArgs e)
+        {
+            ExecuteXPath("//contact"); //TODO
+        }
+
+        protected void ButtonOnlyMaleInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSpecificLastName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
